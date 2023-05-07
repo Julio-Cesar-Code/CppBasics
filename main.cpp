dc14e870c18a899daa3989e59a1298834158e054
Julio-Cesar-Code <julio111499@gmail.com>
2023-05-07 10:10:55 -0500

    finish the basics course

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    const string name ="Julio";
+    cout << "Hola "+ name<<endl;
+    cout << "El mas calidoso es: "+ name<<endl;
+    int ages[]={21,23,25,26};
+    cout << ages[0]<<endl;
+}
---
+++ operandplus.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  /*const int a = 45;
+  const int b = 30;
+  const int NUMBERS[] = {24, 40, 7};
+
+  const int sum = a + b;
+  const int res = a - b;
+  const int div = a / b;
+  const bool mayo = a > b;
+
+  cout << sum << endl;
+  cout << res << endl;
+  cout << div << endl;
+  cout << mayo << endl;
+  cout << sizeof(NUMBERS) << endl;*/
+  int edad =0;
+  cout << "Introduce tu edad: ";
+  cin >> edad; 
+  cout << "Tu edad es: "<< edad; 
+}
